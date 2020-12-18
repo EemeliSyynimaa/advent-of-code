89c9d531dba8c517c182b921b3cb68eeff315e89
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2020-12-18 08:50:33 +0200

    Add 2020 day 8 solution

---
+++ source/8b.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_MAX 256
+
+struct command
+{
+    char operation[4];
+    int value;
+    int times_executed;
+};
+
+void operation_swap(char* operation)
+{
+    if (!strcmp("jmp", operation))
+    {
+        strcpy(operation, "nop");
+    }
+    else if (!strcmp("nop", operation))
+    {
+        strcpy(operation, "jmp");
+    }   
+}
+
+int main(int argc, char** argv)
+{
+    FILE* fp = fopen("../data/8.txt", "r");
+
+    if (fp)
+    {
+        char buf[BUFFER_MAX];
+        int rows = 0;
+
+        while (fgets(buf, BUFFER_MAX, fp))
+        {
+            rows++;
+        }
+
+        printf("Number of rows in input file: %d\n", rows);
+
+        struct command* commands = malloc(sizeof(struct command) * rows);
+
+        rewind(fp);
+
+        int command_count = 0;
+
+        while (fgets(buf, BUFFER_MAX, fp))
+        {
+            struct command* temp = &commands[command_count++];
+
+            sscanf(buf, "%s %d", temp->operation, &temp->value);
+
+            temp->times_executed = 0;
+        }
+
+        int accumulator = 0;
+        int commands_executed = 0;
+        int invalid_command_found = 0;
+
+        for (int i = 0; i < command_count && !invalid_command_found; i++)
+        {
+            accumulator = 0;
+
+            if (!strcmp("acc", commands[i].operation))
+            {
+                continue;
+            }
+            
+            operation_swap(commands[i].operation);
+
+            struct command* head = &commands[0];
+            struct command* tail = &commands[command_count-1];
+            struct command* temp = head;
+
+            while (!temp->times_executed)
+            {
+                commands_executed++;
+                temp->times_executed++;
+
+                if (!strcmp("jmp", temp->operation))
+                {
+                    temp += temp->value; 
+                }
+                else if (!strcmp("acc", temp->operation))
+                {
+                    accumulator += temp->value;
+
+                    temp++;
+                }
+                else if (!strcmp("nop", temp->operation))
+                {
+                    temp++;
+                }
+
+                if (temp > tail || temp < head)
+                {
+                    printf("%s\n", temp->operation);
+                    invalid_command_found = 1;
+                    break;
+                }
+            }
+
+            for (int j = 0; j < command_count; j++)
+            {
+                commands[j].times_executed = 0;
+            }
+
+            operation_swap(commands[i].operation);
+        }
+
+        printf("Accumulator: %d\n", accumulator);
+    }
+    else
+    {
+        printf("file not found\n");
+    }
+
+    return 0;
+}
